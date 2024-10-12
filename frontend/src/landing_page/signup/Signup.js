a63b344479220ebcc5105ada1aabbb238fa93e5d
aniketyadav66 <aniketkumar9134@gmail.com>
2024-10-12 23:22:40 +0530

    Initial commit

---
+++ frontend/src/landing_page/signup/Signup.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+function Signup() {
+    return ( 
+
+        <div className='container p-5 mb-5'>
+            <div className='row'>
+                <div className='col-4 mb-2'>
+                  <img src='media\images\signup.png' style={{width:"120%"}}/>
+                </div>
+                <div className='col-2'>
+
+                </div>
+                <div className='col-6'>
+                <h1 className='mb-3 fs-1 text-muted'>Signup now</h1>
+                    <p>Or track your existing application.</p>
+                <input className='number'>
+                
+                </input>
+            
+          <p className='mt-5'>
+          You will receive an OTP on your number
+          </p>
+          <button className='p-2 btn btn-primary fs-5 mb-5' style={{width:"20%",margin:"0 auto"}}>Create</button>
+
+                </div>
+            </div>
+            <p>I authorise Zerodha to contact me even if my number is registered on DND. I authorise Zerodha to fetch my KYC information from the C-KYC registry with my PAN.<br/> Please visit this article to know more.</p>
+            <p>If you are looking to open a HUF, Corporate, Partnership, or NRI account, you have to use the offline forms. For help, click here.</p>
+        </div>
+     );
+}
+
+export default Signup;
---
+++ frontend/src/landing_page/support/SupportPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Hero from './Hero'
+import CreatePage from './CreateTicket'
+
+function SupportPage() {
+    return (  
+        <>
+        <Hero/>
+        <CreatePage/>
+        </>
+    );
+}
+
+export default SupportPage;
